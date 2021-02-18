088193cd1602893f20330e38c8c4ee86e45612e2
Erik Wernersson <erik.wernersson@gmail.com>
2021-02-18 12:14:07 +0100

    ...

--- px_classify_region.m
+++ px_classify_region.m
@@ -4,15 +4,18 @@ function class = px_classify_region(Mdl, I)
 F = px_features_2d(I);
 Q = reshape(F, [size(F,1)*size(F,2), size(F,3)]);
 
-
-if 1
+if 0
     warning('Experimental stuff, don''t use!')
+    tic
     class = cMdl(Q');
     class = reshape(class, size(I));
-else 
+    toc
+else
+    tic
     classification = Mdl.predict(Q);
     class = cellfun(@(x) str2num(x), classification);
     class = reshape(class, size(I));
+    toc
 end
 
 end
